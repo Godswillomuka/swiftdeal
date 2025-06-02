4f6ca748a7cb8f9d40218eb9323b6202eadf8254
Godswill omuka <omukagodswil@gmail.com>
2025-06-03 00:33:36 +0300

    Adjust mobile navbar button alignment using margin-left: 50px

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+    if (!menuOpen) {
+      document.body.style.overflow = 'hidden'; // prevent scroll when menu open
+    } else {
+      document.body.style.overflow = 'auto'; // re-enable scroll
+    }
+  };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
   return (
     <nav className="navbar">
-      {/* Logo wrapped in Link */}
+      {/* Logo */}
       <div className="nav-logo">
-        <Link to="/" className="nav-logo-link" style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Link
+          to="/"
+          className="nav-logo-link"
+          onClick={closeMenu}
+          style={{ textDecoration: 'none', color: 'inherit' }}
+        >
           <h1>SwiftDeal</h1>
           <span className="nav-sublogo">-Global Marketplace-</span>
         </Link>
       </div>
 
-      {/* Navigation Links */}
-      <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About Us</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/sell">Sell</Link></li>
-      </ul>
+      {/* Hamburger menu */}
+      <div className={`hamburger ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className={`bar ${menuOpen ? 'change' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'change' : ''}`}></div>
+        <div className={`bar ${menuOpen ? 'change' : ''}`}></div>
+      </div>
 
-      {/* Buttons */}
-      <div className="nav-buttons">
-        <button className="login-button">Sign In</button>
-        <button className="signup-button">Register</button>
+      {/* Nav menu container (links + buttons) */}
+      <div className={`nav-menu ${menuOpen ? 'nav-active' : ''}`}>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" onClick={closeMenu}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/about" onClick={closeMenu}>
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" onClick={closeMenu}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link to="/sell" onClick={closeMenu}>
+              Sell
+            </Link>
+          </li>
+        </ul>
+
+        <div className="nav-buttons">
+          <button className="login-button">Sign In</button>
+          <button className="signup-button">Register</button>
+        </div>
       </div>
     </nav>
   );
